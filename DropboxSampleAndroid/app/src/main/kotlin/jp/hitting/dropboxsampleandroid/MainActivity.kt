5f6af816b1dd802efa83174104b48fe43316278f
IOKA <hit0apps@gmail.com>
2017-05-03 21:45:43 +0900

    login process

--- DropboxSampleAndroid/app/src/main/kotlin/jp/hitting/dropboxsampleandroid/MainActivity.kt
+++ DropboxSampleAndroid/app/src/main/kotlin/jp/hitting/dropboxsampleandroid/MainActivity.kt
@@ -1,7 +1,11 @@
 package jp.hitting.dropboxsampleandroid
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.dropbox.core.DbxRequestConfig
+import com.dropbox.core.android.Auth
+import com.dropbox.core.http.OkHttp3Requestor
+import com.dropbox.core.v2.DbxClientV2
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +13,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (dropboxClient == null) {
+            val accessToken = Auth.getOAuth2Token()
+            if (accessToken != null) {
+                initDropboxClient(accessToken)
+            }
+        }
+    }
+
+    private fun login() {
+        Auth.startOAuth2Authentication(this, this.getString(R.string.dropbox_app_key))
+    }
+
+    companion object {
+
+        var dropboxClient: DbxClientV2? = null
+
+        fun initDropboxClient(accessToken: String) {
+            if (dropboxClient != null) {
+                return
+            }
+            val config = DbxRequestConfig.newBuilder("DropboxSampleAndroid")
+                    .withHttpRequestor(OkHttp3Requestor.INSTANCE)
+                    .build()
+            dropboxClient = DbxClientV2(config, accessToken)
+        }
+
+    }
+
 }
